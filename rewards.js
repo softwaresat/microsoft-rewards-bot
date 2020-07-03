beca723b99fb2e8dd06bcbb1e61f0943aecf8dd5
softwaresat <ag.satvik@gmail.com>
2020-07-03 11:16:17 -0500

    Update rewards.js
    Added Status Messages

--- rewards.js
+++ rewards.js
@@ -28,7 +28,8 @@ var speed;
 const puppeteer = require('puppeteer');
 setTimeout(
     async function () {
-    
+        console.log('Currently Logging You In!')
+
     const browser =  await puppeteer.launch();    
     const page =  await browser.newPage();
     await page.goto('https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1593542743&rver=6.7.6631.0&wp=MBI_SSL&wreply=https%3a%2f%2fwww.bing.com%2fsecure%2fPassport.aspx%3frequrl%3dhttps%253a%252f%252fwww.bing.com%252fsearch%253fq%253daaaa%2526go%253dSearch%2526qs%253dds%2526form%253dQBRE%2526wlsso%253d1%2526wlexpsignin%253d1%26sig%3d1EB29EC896D9682B3684903197676976&lc=1033&id=264960&CSRFToken=0fac2379-af43-43a7-9152-fc72ff38f12b&aadredir=1')
@@ -39,6 +40,7 @@ setTimeout(
          await page.click('#idChkBx_PWD_KMSI0Pwd',{delay:500})
         await page.click('#idSIButton9', {delay:1000});
     
+      console.log('Currently Searching!')
 
     for(i=1;i<35;i++){
         
@@ -49,7 +51,7 @@ setTimeout(
     }
     
    
-   console.log('done')
+   console.log('Done with the work!')
   await browser.close()
 
 }, 20000)
